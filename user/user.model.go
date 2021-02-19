1a7c0297199bd4777a5ac11e86057e324684f04f
hpdipto <haris.dipto@gmail.com>
2021-02-19 18:08:04 +0600

    struct json updated

--- user/user.model.go
+++ user/user.model.go
@@ -7,14 +7,14 @@ import (
 
 // User struct type
 type User struct {
-	ID           uint       `json:"id"`
-	FirstName    string     `json:"first_name"`
-	LastName     string     `json:"last_name"`
-	Email        string     `json:"email"`
-	Age          uint8      `json:"age"`
-	Birthday     *time.Time `json:"birth_day"`
-	MemberNumber sql.NullString
-	ActivatedAt  sql.NullTime
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	ID           uint           `json:"id"`
+	FirstName    string         `json:"first_name"`
+	LastName     string         `json:"last_name"`
+	Email        string         `json:"email"`
+	Age          uint8          `json:"age"`
+	Birthday     *time.Time     `json:"birth_day"`
+	MemberNumber sql.NullString `json:"-"`
+	ActivatedAt  sql.NullTime   `json:"-"`
+	CreatedAt    time.Time      `json:"-"`
+	UpdatedAt    time.Time      `json:"-"`
 }
